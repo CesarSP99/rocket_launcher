051c70afaca646915cd47d8d3782a4c559de6cf8
Cesar Salazar <cesarandres00@gmail.com>
2022-08-23 15:52:48 -0400

    Add HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'views/launch_page.dart';
+import 'views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LaunchPage(title: 'Rocket Launcher'),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/views/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'launch_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? selectedAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: const Color(0xff0b5842),
+        backgroundColor: const Color(0xff89c970),
+        title: const Text('Rocket Launcher UPSA'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Seleccione el puerto serial a usar:'),
+            DropdownButton(
+              value: selectedAddress,
+              items: SerialPort.availablePorts
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (String? selection) {
+                setState(() {
+                  selectedAddress = selection;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 40,
+              width: 120,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0xff89c970),
+                ),
+                onPressed: selectedAddress == null
+                    ? null
+                    : () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => LaunchPage(
+                              port: SerialPort(selectedAddress!),
+                            ),
+                          ),
+                        );
+                      },
+                child: Row(
+                  children: const [
+                    Text("Siguiente"),
+                    Icon(Icons.arrow_right_alt_outlined),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
