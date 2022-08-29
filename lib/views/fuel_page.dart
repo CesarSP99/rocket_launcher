b112b37fd2cf2b5063ce6cea18dc67e73236cf4b
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-08-29 17:17:17 -0400

    Update FuelPage styles and background

--- lib/views/fuel_page.dart
+++ lib/views/fuel_page.dart
@@ -44,153 +44,181 @@ class _FuelPageState extends State<FuelPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Rocket Launcher",
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text(
+            "Rocket Launcher",
+          ),
         ),
-      ),
-      body: Column(
-        children: [
-          const FittedBox(
-            child: Padding(
-              padding: EdgeInsets.only(top: 20),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const FittedBox(
               child: Text(
                 "Lanzador de Cohetes UPSA",
-                style: TextStyle(fontSize: 30),
+                style: TextStyle(fontSize: 50),
               ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: Center(
-                      child: GestureDetector(
-                          onLongPressStart: (_) {
-                            setState(() {
-                              _isPressed = true;
-                            });
-                            fuelRocket();
-                          },
-                          onLongPressEnd: (_) {
-                            setState(() {
-                              _isPressed = false;
-                            });
-                          },
-                          child: Lottie.asset(
-                            'assets/lottie/manguera.json',
-                            width: 180,
-                          )),
-                    ),
+            (_isPressed)
+                ? const FittedBox(
+                    child: Text(
+                    'Cargando H2 Verde...',
+                    style: TextStyle(fontSize: 30),
+                  ))
+                : const SizedBox(
+                    height: 35,
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: Lottie.asset(
-                      'assets/lottie/cohete1.json',
-                      width: 550,
-                    ),
-                  ),
-                  Flexible(
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
                       flex: 1,
                       child: Center(
-                        child: ConstrainedBox(
-                            constraints: const BoxConstraints(maxWidth: 250),
-                            child: RadialGauge(
-                              axes: [
-                                RadialGaugeAxis(
-                                    color: Colors.transparent,
-                                    minValue: -100,
-                                    maxValue: 100,
-                                    minAngle: -150,
-                                    maxAngle: 150,
-                                    radius: 0.6,
-                                    width: 0.2,
-                                    segments: [
-                                      const RadialGaugeSegment(
-                                        minValue: 0,
-                                        maxValue: 20,
-                                        minAngle: -150,
-                                        maxAngle: -90,
-                                        color: Colors.red,
-                                      ),
-                                      const RadialGaugeSegment(
-                                        minValue: 20,
-                                        maxValue: 40,
-                                        minAngle: -90,
-                                        maxAngle: -30,
-                                        color: Colors.orange,
-                                      ),
-                                      const RadialGaugeSegment(
-                                        minValue: 40,
-                                        maxValue: 60,
-                                        minAngle: -30,
-                                        maxAngle: 30,
-                                        color: Colors.yellow,
-                                      ),
-                                      const RadialGaugeSegment(
-                                        minValue: 60,
-                                        maxValue: 80,
-                                        minAngle: 30,
-                                        maxAngle: 90,
-                                        color: Colors.lightGreen,
-                                      ),
-                                      const RadialGaugeSegment(
-                                        minValue: 80,
-                                        maxValue: 100,
-                                        minAngle: 90,
-                                        maxAngle: 150,
-                                        color: Colors.green,
-                                      ),
-                                    ],
-                                    pointers: [
-                                      RadialNeedlePointer(
-                                          value: _fuelPointer,
-                                          thicknessStart: 20,
-                                          thicknessEnd: 0,
-                                          length: 0.6,
-                                          knobRadiusAbsolute: 10,
-                                          gradient: const LinearGradient(
-                                              colors: [
-                                                Colors.black,
-                                                Colors.grey
-                                              ]))
-                                    ]),
+                        child: GestureDetector(
+                            onLongPressStart: (_) {
+                              setState(() {
+                                _isPressed = true;
+                              });
+                              fuelRocket();
+                            },
+                            onLongPressEnd: (_) {
+                              setState(() {
+                                _isPressed = false;
+                              });
+                            },
+                            child: Column(
+                              children: [
+                                Lottie.asset(
+                                  'assets/lottie/manguera.json',
+                                  width: 180,
+                                ),
+                                Center(
+                                    child: (!_isPressed)
+                                        ? const Text('Presiona aquí!')
+                                        : null)
                               ],
                             )),
-                      ))
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              // ElevatedButton(
-              //   style: ElevatedButton.styleFrom(
-              //     primary: Colors.red,
-              //     shape: const CircleBorder(),
-              //     padding: const EdgeInsets.all(20),
-              //   ),
-              //   onPressed: () {
-              //     Navigator.of(context).push(
-              //       MaterialPageRoute(
-              //         builder: (context) => IgnitionPage(
-              //           port: widget.port,
-              //         ),
-              //       ),
-              //     );
-              //   },
-              //   child: const Icon(
-              //     Icons.rocket_launch,
-              //     size: 50,
-              //   ),
-              // ),
-            ],
-          ),
-        ],
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Lottie.asset(
+                        'assets/lottie/cohete1.json',
+                        width: 550,
+                      ),
+                    ),
+                    Flexible(
+                        flex: 1,
+                        child: Center(
+                          child: ConstrainedBox(
+                              constraints: const BoxConstraints(maxWidth: 250),
+                              child: Column(
+                                children: [
+                                  RadialGauge(
+                                    axes: [
+                                      RadialGaugeAxis(
+                                          color: Colors.transparent,
+                                          minValue: -100,
+                                          maxValue: 100,
+                                          minAngle: -150,
+                                          maxAngle: 150,
+                                          radius: 0.6,
+                                          width: 0.2,
+                                          segments: [
+                                            const RadialGaugeSegment(
+                                              minValue: 0,
+                                              maxValue: 20,
+                                              minAngle: -150,
+                                              maxAngle: -90,
+                                              color: Colors.red,
+                                            ),
+                                            const RadialGaugeSegment(
+                                              minValue: 20,
+                                              maxValue: 40,
+                                              minAngle: -90,
+                                              maxAngle: -30,
+                                              color: Colors.orange,
+                                            ),
+                                            const RadialGaugeSegment(
+                                              minValue: 40,
+                                              maxValue: 60,
+                                              minAngle: -30,
+                                              maxAngle: 30,
+                                              color: Colors.yellow,
+                                            ),
+                                            const RadialGaugeSegment(
+                                              minValue: 60,
+                                              maxValue: 80,
+                                              minAngle: 30,
+                                              maxAngle: 90,
+                                              color: Colors.lightGreen,
+                                            ),
+                                            const RadialGaugeSegment(
+                                              minValue: 80,
+                                              maxValue: 100,
+                                              minAngle: 90,
+                                              maxAngle: 150,
+                                              color: Colors.green,
+                                            ),
+                                          ],
+                                          pointers: [
+                                            RadialNeedlePointer(
+                                                value: _fuelPointer,
+                                                thicknessStart: 20,
+                                                thicknessEnd: 0,
+                                                length: 0.6,
+                                                knobRadiusAbsolute: 10,
+                                                gradient: const LinearGradient(
+                                                    colors: [
+                                                      Colors.black,
+                                                      Colors.grey
+                                                    ]))
+                                          ]),
+                                    ],
+                                  ),
+                                ],
+                              )),
+                        ))
+                  ],
+                ),
+                // const SizedBox(
+                //   height: 30,
+                // ),
+                // ElevatedButton(
+                //   style: ElevatedButton.styleFrom(
+                //     primary: Colors.red,
+                //     shape: const CircleBorder(),
+                //     padding: const EdgeInsets.all(20),
+                //   ),
+                //   onPressed: () {
+                //     Navigator.of(context).push(
+                //       MaterialPageRoute(
+                //         builder: (context) => IgnitionPage(
+                //           port: widget.port,
+                //         ),
+                //       ),
+                //     );
+                //   },
+                //   child: const Icon(
+                //     Icons.rocket_launch,
+                //     size: 50,
+                //   ),
+                // ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
